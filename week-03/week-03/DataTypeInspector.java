4f05d21d1417bd718f1c1357fb6ba67fe1727589
shudhanshuthakur01-creator <shudhanshuthakur01@gmail.com>
2026-03-08 23:11:43 +0545

    Update DataTypeInspector.java

--- week-03/week-03/DataTypeInspector.java
+++ week-03/week-03/DataTypeInspector.java
@@ -1,1 +1,24 @@
+public class DataTypeInspector {
+
+    public static void main(String[] args) {
+
+        byte B = 10;
+        short S = 200;
+        int I = 1000;
+        long L = 10000L;
+        float F = 5.5f;
+        double D = 20.25;
+        char C = '@';
+        boolean B = true;
+
+        System.out.println("Byte value: " +B);
+        System.out.println("Short value: " +S);
+        System.out.println("Int value: " +I);
+        System.out.println("Long value: " +L);
+        System.out.println("Float value: " +F);
+        System.out.println("Double value: " +D);
+        System.out.println("Char value: " +C);
+        System.out.println("Boolean value: " +B);
+    }
+}
 
