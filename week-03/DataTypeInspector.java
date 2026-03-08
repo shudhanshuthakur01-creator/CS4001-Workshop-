30ecd846264f411daae9f7a41c3e2ad7ac8b48e8
shudhanshuthakur01-creator <shudhanshuthakur01@gmail.com>
2026-03-08 22:51:31 +0545

    Update DataTypeInspector.java

--- week-03/DataTypeInspector.java
+++ week-03/DataTypeInspector.java
@@ -2,26 +2,33 @@ public class DataTypeInspector {
 
     public static void main(String[] args) {
 
-byte Byte = 10;
-short Short = 200;
-int Int = 1000;
-long Long = 10000L;
+        byte Byte = 10;
 
-float Float = 5.5f;
-double Double = 20.25;
+        short Short = 200;
 
-char Char = '@';
-boolean Boolean = true;
+        int Int = 1000;
 
-        System.out.println("Byte value: " + Byte);
-        System.out.println("Short value: " + Short);
-        System.out.println("Int value: " + Int);
-        System.out.println("Long value: " + Long);
+        long Long = 10000L;
 
-        System.out.println("Float value: " + Float);
-        System.out.println("Double value: " + Double);
 
-        System.out.println("Char value: " + Char);
-        System.out.println("Boolean value: " + Boolean);
+        float Float = 5.5f;
+
+        double Double = 20.25;
+
+
+        char Char = '@';
+
+        boolean Boolean = true;
+
+        System.out.println("Byte value: " +Byte);
+        System.out.println("Short value: " +Short);
+        System.out.println("Int value: " +Int);
+        System.out.println("Long value: " +Long);
+
+        System.out.println("Float value: " +Float);
+        System.out.println("Double value: " +Double);
+
+        System.out.println("Char value: " +Char);
+        System.out.println("Boolean value: " +Boolean);
     }
 }
