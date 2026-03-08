98aa440a3a2c536af5b9ecebd1916a6b10f0cc98
shudhanshuthakur01-creator <shudhanshuthakur01@gmail.com>
2026-03-08 22:41:52 +0545

    Update LiteralPractice.java

--- week-03/LiteralPractice.java
+++ week-03/LiteralPractice.java
@@ -1,1 +1,15 @@
+public class Literalpractice {
 
+    public static void main(String[] args) {
+
+        long number = 123456789L;
+
+        float price = 12.5f;
+
+        char symbol = '\u00A9';
+
+        System.out.println("Long value: " + number);
+        System.out.println("Float value: " + price);
+        System.out.println("Unicode char: " + symbol);
+    }
+}
