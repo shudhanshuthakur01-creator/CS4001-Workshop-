569797393a39c72165a2aef1b855cf5cdc2a37d3
shudhanshuthakur01-creator <shudhanshuthakur01@gmail.com>
2026-03-08 22:41:25 +0545

    Update GradeEvaluator.java

--- week-03/GradeEvaluator.java
+++ week-03/GradeEvaluator.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class GradeEvaluator {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int grade;
+        
+        // this variable stores grade
+        System.out.println("Enter your grade:");
+        grade = input.nextInt();
+
+        if (grade >= 40) {
+          System.out.println("Pass");
+               } else {
+          System.out.println("Fail");
+            }
+
+        input.close();
+    }
+}
